d152fc56ca9832d618a54d24f4fa4c88d0c96cbf
William Kvaale <wgkvaale@hotmail.com>
2017-11-07 12:48:17 +0100

    Implemented motob

--- basic_robot/motob.py
+++ basic_robot/motob.py
@@ -1,18 +1,34 @@
+__author__ = 'William Kvaale'
+from basic_robot.motors import Motors
+
+# The Arbitrator will decide which motobs that will be executed
+# Motobs tell the Motors what to do
+# Motors execute what's it told. How glorious!
 
 
-#Arbitratoren bestemmer hvilke motobs som skal utføres. Motobs gir beskjed til motors hva de skal gjøre
 class Motob:
 
     def __init__(self):
-        self.motors = [] #En liste av motorer som sine handlinger vil avgjøres av motob
-        self.value = 0 #a holder of the most recent motor recommendation sent to the motob
+        self.motor = Motors()  # Initializing Motors, which motob will make decisions for
+        self.value = []  # a holder of the most recent motor recommendation sent to the motob
 
-
-    def update(self):
-        #Motta en motor recommendation, load til value, og operationalize det
-        pass
+    def update(self, mr):
+        # Receive a motor recommendation (mr), load into value, and operationalize!
+        self.value = mr  # [direction, speed, duration]
+        self.operationalize()
 
     def operationalize(self):
-        #Convert en motor recommendation til en eller flere motor settings, som er sendt til de korresponderene motorene
-        pass
+        # Convert a motor recommendation into on or more motor settings,
+        # which is sent back to the corresponding motors
+        motor = self.motor
+        for val in self.value:
+            if val[0] == 'F':
+                motor.forward(val[1], val[2])
+            elif val[0] == 'B':
+                motor.backward(val[1], val[2])
+            elif val[0] == 'R':
+                motor.right(val[1], val[2])
+            elif val[0] == 'L':
+                motor.left(val[1], val[2])
+
 
